6b030e719ba1c80231499b19c4fea447d5bef36f
Thiago Perrotta <thiago@perrotta.dev>
2025-10-30 12:54:24 +0100

    formula: refactor and fix ssh - -> _

--- Formula/pancake.rb
+++ Formula/pancake.rb
@@ -1,34 +1,30 @@
 class Pancake < Formula
-  desc "A collection of useful shell scripts"
+  desc "Collection of useful shell scripts"
   homepage "https://github.com/thiagowfx/pancake"
   head "https://github.com/thiagowfx/pancake.git"
 
-  def install
+  # Script definitions: [directory, script_file, command_name]
+  SCRIPTS = [
     # keep-sorted start
-    bin.install "aws_china_mfa/aws_china_mfa.sh" => "aws_china_mfa"
-    bin.install "img_optimize/img_optimize.sh" => "img_optimize"
-    bin.install "op_login_all/op_login_all.sh" => "op_login_all"
-    bin.install "pritunl_login/pritunl_login.sh" => "pritunl_login"
-    bin.install "sd_world/sd_world.sh" => "sd_world"
-    bin.install "ssh_mux_restart/ssh_mux_restart.sh" => "ssh-mux-restart"
+    ["aws_china_mfa", "aws_china_mfa.sh", "aws_china_mfa"],
+    ["img_optimize", "img_optimize.sh", "img_optimize"],
+    ["op_login_all", "op_login_all.sh", "op_login_all"],
+    ["pritunl_login", "pritunl_login.sh", "pritunl_login"],
+    ["sd_world", "sd_world.sh", "sd_world"],
+    ["ssh_mux_restart", "ssh_mux_restart.sh", "ssh_mux_restart"],
     # keep-sorted end
+  ].freeze
+
+  def install
+    SCRIPTS.each do |dir, script, command|
+      bin.install "#{dir}/#{script}" => command
+    end
   end
 
   test do
-    # Basic test to ensure the scripts are installed and executable.
-    # keep-sorted start
-    assert_predicate bin/"aws_china_mfa", :executable?
-    assert_predicate bin/"aws_china_mfa", :exist?
-    assert_predicate bin/"img_optimize", :executable?
-    assert_predicate bin/"img_optimize", :exist?
-    assert_predicate bin/"op_login_all", :executable?
-    assert_predicate bin/"op_login_all", :exist?
-    assert_predicate bin/"pritunl_login", :executable?
-    assert_predicate bin/"pritunl_login", :exist?
-    assert_predicate bin/"sd_world", :executable?
-    assert_predicate bin/"sd_world", :exist?
-    assert_predicate bin/"ssh-mux-restart", :executable?
-    assert_predicate bin/"ssh-mux-restart", :exist?
-    # keep-sorted end
+    SCRIPTS.each do |_, _, command|
+      assert_path_exists bin/command
+      assert_predicate bin/command, :executable?
+    end
   end
 end
